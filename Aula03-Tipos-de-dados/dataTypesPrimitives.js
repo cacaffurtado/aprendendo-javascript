9da9a3341205c09b7afb22997a2b28f909f320bf
Ana Carolina <cacaffurtado15@gmail.com>
2025-03-12 21:33:39 -0300

    feat: aula 3 - tipos de dados primitivos

---
+++ Aula03-Tipos-de-dados/dataTypesPrimitives.js
@@ -0,0 +1,42 @@
+// Tipagem dinâmica
+
+// Number
+const idade = 22;
+
+// String
+const nome = "Carolina"
+
+// Boolean
+let cabeloLoiro = true;
+console.log(`${nome} tem cabelo loiro?`, cabeloLoiro)
+
+console.log("Muitos anos depois...")
+
+cabeloLoiro = false;
+console.log(`${nome} tem cabelo loiro?`, cabeloLoiro)
+
+// Undefined -- adicionar valor a variável
+// desempregada
+let profissao;
+console.log(profissao);
+//empregada
+profissao = "QA";
+console.log(`A profissão de ${nome} é`, profissao);
+
+// Null -- tirar valor de variável
+let carro = "Fusca";
+console.log(`Hoje, o carro de ${nome} é um`, carro);
+
+console.log("Muitos anos depois...")
+
+carro = "Honda Civic";
+console.log(`Agora, o carro de ${nome} é um`, carro);
+
+console.log("Perdeu o emprego...")
+
+carro = null;
+console.log(`O carro de ${nome} é um`, carro);
+
+// Symbol -- para variáveis imutáveis
+
+// BigInt -- para números muito grandes, que o number não pode armazenar
