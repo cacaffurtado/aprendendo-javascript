2676bb3c8f99a700dacec55f9da2c21b4692305b
Ana Carolina <cacaffurtado15@gmail.com>
2025-03-15 12:57:17 -0300

    feat: aula 8 - loops

---
+++ Aula08-Loops/loops.js
@@ -0,0 +1,21 @@
+// LOOPS
+
+// for
+for(let dia = 1; dia <= 30; dia++){
+    console.log("Dia", dia);
+}
+
+// while
+// let dia = 1;
+
+// while(dia <=30){
+//     console.log("Dia", dia);
+//     dia++;
+// }
+
+// do while
+// let dia = 1;
+// do{
+//     console.log("Dia", dia);
+//     dia++;
+// } while (dia <= 30);
